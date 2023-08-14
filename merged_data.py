edca8a235a79b40f8834728a85fa9793bb009a42
Parker Huang <parkerdhuang@gmail.com>
2023-08-14 00:34:06 -0700

    merged data

--- merged_data.py
+++ merged_data.py
@@ -9,10 +9,12 @@ twitch_data = pd.read_csv(twitch)
 player = pd.read_csv(player_data)
 
 
-def merged_data(game_data: pd.DataFrame, twitch_data: pd.DataFrame, player = pd.DataFrame) -> pd.DataFrame:
+def merged_data(game_data: pd.DataFrame, twitch_data: pd.DataFrame,
+                player=pd.DataFrame) -> pd.DataFrame:
     game_data['name'] = game_data['name'].str.rstrip('®')
     player['Game_Name'] = player['Game_Name'].str.replace(
-           'Counter Strike: Global Offensive', 'Counter-Strike: Global Offensive')
+           'Counter Strike: Global Offensive',
+           'Counter-Strike: Global Offensive')
     player['Game_Name'] = player['Game_Name'].str.replace(
         'PUBG: Battlegrounds', "PLAYERUNKNOWN'S BATTLEGROUNDS")
 
@@ -33,20 +35,23 @@ def merged_data(game_data: pd.DataFrame, twitch_data: pd.DataFrame, player = pd.
                                            'Peak_viewers', 'Avg_viewers',
                                            'Hours_watched']]
 
-    player[['month', 'year']] = player['Month_Year'].str.split(' ', expand=True)
+    player[['month', 'year']] = player['Month_Year'].str.split(' ',
+                                                               expand=True)
     player['year'] = pd.to_numeric(player['year'])
 
-    month_mapping = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5,
-                     'June': 6, 'July': 7, 'August': 8, 'September': 9,
-                     'October': 10, 'November': 11, 'December': 12}
-   
+    month_mapping = {'January': 1, 'February': 2, 'March': 3, 'April': 4,
+                     'May': 5, 'June': 6, 'July': 7, 'August': 8,
+                     'September': 9, 'October': 10, 'November': 11,
+                     'December': 12}
+
     player['month'] = player['month'].map(month_mapping)
     player_years = player[player['year'] >= 2017]
     player_years_filtered = player_years[player_years
                                          ['Game_Name'].isin(
                                             game_names_to_filter)]
     player_data_filtered = player_years_filtered[['Game_Name', 'month', 'year',
-                                                  'Percent_Gain', 'Avg_players',
+                                                  'Percent_Gain',
+                                                  'Avg_players',
                                                   'Peak_Players']]
 
     merged_games = games_filtered.merge(player_data_filtered, left_on='name',

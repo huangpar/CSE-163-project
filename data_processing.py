90547865739faeb24ba6a1b40f0c63c9468b73d1
etzwang <102839222+etzwang@users.noreply.github.com>
2023-08-16 11:44:08 -0700

    Add files via upload

--- data_processing.py
+++ data_processing.py
@@ -7,13 +7,11 @@ def cleanse_twitch_data(twitch_data: pd.DataFrame) -> pd.DataFrame:
                             'Stardew Valley', 'Rocket League',
                             'Cyberpunk 2077', 'Apex Legends']
 
-    twitch_data = twitch_data[(twitch_data['Year'] >= 2017) &
-                        (twitch_data['Year'] <= 2021)]
-    twitch_data['Year-Month'] = twitch_data['Year'].astype(str) + "-" + twitch_data['Month'].astype(str)
+    twitch_data = twitch_data[(twitch_data['Year'] >= 2017) & (twitch_data['Year'] <= 2021)]
+    # twitch_data['Year-Month'] = twitch_data['Year'].astype(str) + "-" + twitch_data['Month'].astype(str)
     twitch_data = twitch_data[twitch_data['Game'].isin(game_names_to_filter)]
-    twitch_data = twitch_data[['Game', 'Month', 'Year', 'Year-Month',
-                                           'Peak_viewers', 'Avg_viewers',
-                                           'Hours_watched']]
+    twitch_data = twitch_data[['Game', 'Month', 'Year', # 'Year-Month',
+                                'Peak_viewers', 'Avg_viewers', 'Hours_watched']]
     return twitch_data
     
 def cleanse_player_data(player_data: pd.DataFrame) -> pd.DataFrame:
@@ -28,23 +26,20 @@ def cleanse_player_data(player_data: pd.DataFrame) -> pd.DataFrame:
     player_data['Game_Name'] = player_data['Game_Name'].str.replace(
         'PUBG: Battlegrounds', "PLAYERUNKNOWN'S BATTLEGROUNDS")
 
-    player_data[['month', 'year']] = player_data['Month_Year'].str.split(' ',
-                                                               expand=True)
+    player_data[['month', 'year']] = player_data['Month_Year'].str.split(' ', expand=True)
     player_data['year'] = pd.to_numeric(player_data['year'])
-
+    player_data = player_data[(player_data['year'] >= 2017) & (player_data['year'] <= 2021)]
     month_mapping = {'January': 1, 'February': 2, 'March': 3, 'April': 4,
                      'May': 5, 'June': 6, 'July': 7, 'August': 8,
                      'September': 9, 'October': 10, 'November': 11,
                      'December': 12}
 
     player_data['month'] = player_data['month'].map(month_mapping)
-    player_data['year-month'] = player_data['year'].astype(str) + "-" + player_data['month'].astype(str)
-    player_data = player_data[player_data['year'] >= 2017]
+    # player_data['year-month'] = player_data['year'].astype(str) + "-" + player_data['month'].astype(str)
+
     player_data = player_data[player_data['Game_Name'].isin(game_names_to_filter)]
-    player_data = player_data[['Game_Name', 'month', 'year', 'year-month',
-                                                  'Gain',
-                                                  'Avg_players',
-                                                  'Peak_Players']]
+    player_data = player_data[['Game_Name', 'month', 'year', # 'year-month',
+                                'Gain', 'Avg_players', 'Peak_Players']]
     player_data['Gain_Rate'] = player_data['Gain'].fillna(0) / player_data['Avg_players']
 
     return player_data
